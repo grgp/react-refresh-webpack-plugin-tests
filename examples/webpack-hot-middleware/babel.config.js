889363be919c49a80b2e9294927413a9ab925386
grgp <g.a.pitoy@gmail.com>
2023-02-02 11:54:18 +0700

    Works without additional babel config

--- examples/webpack-hot-middleware/babel.config.js
+++ examples/webpack-hot-middleware/babel.config.js
@@ -12,7 +12,8 @@ module.exports = (api) => {
     ],
     // Applies the react-refresh Babel plugin on non-production modes only
     ...(!api.env('production') && {
-      plugins: ['react-hot-loader/babel', 'react-refresh/babel'],
+      // plugins: ['react-hot-loader/babel', 'react-refresh/babel'],
+      plugins: ['react-refresh/babel'],
     }),
   };
 };

ee0cd3fc08e6d50f0b41e1fb40500798b037264f
grgp <g.a.pitoy@gmail.com>
2025-02-17 14:29:51 +0700

    Downgrade to react 17 and react dom render

--- examples/webpack-hot-middleware/src/index.js
+++ examples/webpack-hot-middleware/src/index.js
@@ -1,6 +1,9 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('app');
-const root = createRoot(container);
-root.render(<App />);
+// const root = createRoot(container);
+// root.render(<App />);
+
+ReactDOM.render(<App />, container);
